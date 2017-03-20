38997fe63778c349aee7ee418a1d642d68c03943
anubhavshrimal <anubhavshrimal@gmail.com>
2017-03-20 17:06:26 +0530

    Added addProject Componend

---
+++ FrontEnd/src/app/components/addProject/addProject.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-project',
+  templateUrl: './addProject.component.html',
+  moduleId: module.id
+})
+export class AddProjectComponent  { }
