ec94896f199018a0f68428dc827951c8b7e670fb
anubhavshrimal <anubhavshrimal@gmail.com>
2017-03-27 01:48:46 +0530

    Added insert project api integration

---
+++ FrontEnd/src/app/services/projects/project.service.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=project.service.js.map
---
+++ FrontEnd/src/app/services/projects/projects.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Project } from '../../classTemplates/project/project';
+import { BackendUrlService } from '../backendUrl.service';
+
+@Injectable()
+export class ProjectsService {
+    private headers = new Headers({'Content-Type': 'application/json'});
+    private url = BackendUrlService.url + '/project/insert';
+
+    constructor(private http: Http) {}
+
+    create(project: Project): Promise<Project> {
+        return this.http
+            .post(this.url, JSON.stringify(Project), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json() as Project)
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+}
