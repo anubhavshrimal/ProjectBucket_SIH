1a66de8f7e947316641bf50f1041e5cce373b7de
mohit <mhtsharma948@gmail.com>
2017-04-02 10:07:07 +0530

    admin

--- FrontEnd/src/app/components/adminPanel/adminPanel.component.js
+++ FrontEnd/src/app/components/adminPanel/adminPanel.component.js
@@ -13,10 +13,17 @@ var AdminPanelComponent = (function () {
     function AdminPanelComponent() {
         var data = '[{"Institute":"Swami Keshvanand Institute Of Technology","rating":"12","solutions":"56","university":"RTU"},{"Institute":"Manipal Institute Of Tech.","rating":"10","solutions":"93","university":"Manipal University"},{"Institute":"SIT","rating":"32","solutions":"36","university":"LPU"},{"Institute":"SKT","rating":"62","solutions":"26","university":"RTU"},{"Institute":"Global Institute Of Tech","rating":"92","solutions":"16","university":"RTU"},{"Institute":"OIT","rating":"78","solutions":"16","university":"DU"}]';
         this.data = JSON.parse(data);
+        this.topuser = true;
     }
     AdminPanelComponent.prototype.ngOnInit = function () {
     };
+    AdminPanelComponent.prototype.toggel = function () {
+        this.topuser = true;
+        this.register = false;
+    };
     AdminPanelComponent.prototype.addInstitute = function (username, password, university, college, state, city, email, mobile, address) {
+        this.topuser = false;
+        this.register = true;
         console.log(username, password, state);
         /*this.adminPanel.adminPanel(username, password, university, college, state, city, email, mobile, address).then(data => {
 
--- FrontEnd/src/app/components/adminPanel/adminPanel.component.ts
+++ FrontEnd/src/app/components/adminPanel/adminPanel.component.ts
@@ -6,6 +6,8 @@ import {AdminPanelService} from '../../services/adminPanel/adminPanel.service'
     templateUrl: './adminPanel.component.html'
 })
 export class AdminPanelComponent implements OnInit {
+    topuser: boolean;
+    register: boolean;
     ngOnInit(): void {
     }
 	public data: Object;
@@ -13,9 +15,15 @@ export class AdminPanelComponent implements OnInit {
     constructor(/*private adminPanel: AdminPanelService*/) {
     	var data = '[{"Institute":"Swami Keshvanand Institute Of Technology","rating":"12","solutions":"56","university":"RTU"},{"Institute":"Manipal Institute Of Tech.","rating":"10","solutions":"93","university":"Manipal University"},{"Institute":"SIT","rating":"32","solutions":"36","university":"LPU"},{"Institute":"SKT","rating":"62","solutions":"26","university":"RTU"},{"Institute":"Global Institute Of Tech","rating":"92","solutions":"16","university":"RTU"},{"Institute":"OIT","rating":"78","solutions":"16","university":"DU"}]';
 		 this.data = JSON.parse(data);
+		 this.topuser = true
     }
-
+    toggel(){
+        this.topuser=true;
+        this.register=false;
+    }
     addInstitute(username:string, password: string, university: string, college: string, state: string, city: string, email: string, mobile: string, address: string){
+        this.topuser=false;
+        this.register=true;
         console.log(username, password, state);
         /*this.adminPanel.adminPanel(username, password, university, college, state, city, email, mobile, address).then(data => {
 
