33e49f9cff364d1507dcf4ea979b57963a8d1186
anubhavshrimal <anubhavshrimal@gmail.com>
2017-04-17 22:00:00 +0530

    added add question in cli version

---
+++ frontend-cli/src/app/components/add-question/add-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'add-question',
+  templateUrl: './add-question.component.html',
+  styleUrls: ['./add-question.component.css']
+})
+export class AddQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
