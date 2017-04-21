11b1f6872868592b6ccd0c1664f94741a6199d74
anubhavshrimal <anubhavshrimal@gmail.com>
2017-04-21 22:00:00 +0530

    added edit profile in cli version

---
+++ frontend-cli/src/app/components/edit-profile/edit-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styles: []
+})
+export class EditProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend-cli/src/app/components/edit-project/edit-project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'edit-project',
+  templateUrl: './edit-project.component.html',
+  styles: []
+})
+export class EditProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
