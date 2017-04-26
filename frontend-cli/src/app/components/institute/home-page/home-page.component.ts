6d9b6efe86f1cec3f8a1c1765244c3f812eaacdc
anubhavshrimal <anubhavshrimal@gmail.com>
2017-04-26 22:00:00 +0530

    added institute homepage in cli version

---
+++ frontend-cli/src/app/components/institute/home-page/home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-page',
+  templateUrl: './home-page.component.html',
+  styles: []
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
