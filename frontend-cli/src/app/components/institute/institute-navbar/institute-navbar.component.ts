5d0149df5d3c28d525a2a0ef55f24b6e1e429e3c
anubhavshrimal <anubhavshrimal@gmail.com>
2017-04-27 22:00:00 +0530

    added institute navbar in cli version

---
+++ frontend-cli/src/app/components/institute/institute-navbar/institute-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'institute-navbar',
+  templateUrl: './institute-navbar.component.html',
+  styles: []
+})
+export class InstituteNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
