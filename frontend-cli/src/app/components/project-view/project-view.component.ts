bc55538988d73c78976842539e8c55c0b53869da
anubhavshrimal <anubhavshrimal@gmail.com>
2017-05-01 22:00:00 +0530

    added project view in cli version

---
+++ frontend-cli/src/app/components/project-view/project-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'project-view',
+  templateUrl: './project-view.component.html',
+  styles: []
+})
+export class ProjectViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
