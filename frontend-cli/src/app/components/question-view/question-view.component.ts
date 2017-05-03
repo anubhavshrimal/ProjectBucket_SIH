a7f57ddd955a4271dac3fdd38f6b782c369f90df
anubhavshrimal <anubhavshrimal@gmail.com>
2017-05-03 22:00:00 +0530

    added questions view in cli version

---
+++ frontend-cli/src/app/components/question-view/question-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'question-view',
+  templateUrl: './question-view.component.html',
+  styles: []
+})
+export class QuestionViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
