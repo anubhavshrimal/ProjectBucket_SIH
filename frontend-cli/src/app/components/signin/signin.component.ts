e61771bd5875f0902755bcd2970ce184c0849ea6
anubhavshrimal <anubhavshrimal@gmail.com>
2017-05-04 22:00:00 +0530

    added signin in cli version

---
+++ frontend-cli/src/app/components/signin/signin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'signin',
+  templateUrl: './signin.component.html',
+  styles: []
+})
+export class SigninComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
