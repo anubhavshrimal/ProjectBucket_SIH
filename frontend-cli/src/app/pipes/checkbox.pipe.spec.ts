1b06df7f297af3dfe38b04b0aa20ba454ca5cf80
anubhavshrimal <anubhavshrimal@gmail.com>
2017-05-10 22:00:00 +0530

    added  checkbox pipe

---
+++ frontend-cli/src/app/pipes/checkbox.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CheckboxPipe } from './checkbox.pipe';
+
+describe('CheckboxPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CheckboxPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
