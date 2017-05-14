73119ad0c511641df6dde5a45fe4b7a69aca0bd3
anubhavshrimal <anubhavshrimal@gmail.com>
2017-05-14 22:00:00 +0530

    added tabs filter in cli

--- frontend-cli/src/app/pipes/tabs-filter.pipe.ts
+++ frontend-cli/src/app/pipes/tabs-filter.pipe.ts
@@ -1,12 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
 
-@Pipe({
-  name: 'tabsFilter'
-})
+@Pipe({name: 'tabsFilter'})
 export class TabsFilterPipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return null;
-  }
-
+   transform(items: Array<any>, category: string): Array<any> {
+        return items.filter(item => item.source[0] == _.toLower(category));
+    }
 }
